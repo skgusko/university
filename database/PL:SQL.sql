834bdcb3e6bc543403196c4fd0c99a86eea6acdc
GONOW <87406080+skgusko@users.noreply.github.com>
2023-05-21 21:27:48 +0900

    style: 라인 정리

--- database/PL:SQL.sql
+++ database/PL:SQL.sql
@@ -1,36 +1,36 @@
 -- SET SERVEROUTPUT ON 
 DECLARE
 	x_dept_name varchar(20); 
-    x_id VARCHAR(5); 
-    x_name VARCHAR(20); 
-    x_course_id varchar(8); 
-    x_semester varchar(8); 
-    x_year numeric(4,0); 
-    x_grade VARCHAR(5); 
-    x_credits int;
+	x_id VARCHAR(5); 
+	x_name VARCHAR(20); 
+	x_course_id varchar(8); 
+	x_semester varchar(8); 
+	x_year numeric(4,0); 
+	x_grade VARCHAR(5); 
+	x_credits int;
 	x_title VARCHAR(40); 
-    x_tot_credit int; 
-    x_gpa numeric(4,1); 
-    x_point numeric(2,1);
+	x_tot_credit int; 
+	x_gpa numeric(4,1); 
+	x_point numeric(2,1);
 
 	CURSOR c1 is
 		SELECT dept_name 
-        FROM department 
-        ORDER BY dept_name;
+		FROM department 
+		ORDER BY dept_name;
 	CURSOR c2 is
 		SELECT id, name
 		FROM student
 		WHERE dept_name = x_dept_name 
-        ORDER BY name;
+		ORDER BY name;
 	CURSOR c3 is
 		SELECT title, credits, semester, year, grade
 		FROM takes left outer join course on takes.course_id=course.course_id 
-        WHERE id = x_id
+		WHERE id = x_id
 		ORDER BY year,
 		CASE semester
 			WHEN 'Spring' THEN 1 
-            WHEN 'Summer' THEN 2 
-            WHEN 'Fall' THEN 3 ELSE 4
+			WHEN 'Summer' THEN 2 
+	    		WHEN 'Fall' THEN 3 ELSE 4
 		END ASC;
         
 BEGIN
@@ -38,36 +38,36 @@ BEGIN
 	LOOP
 	FETCH c1 INTO x_dept_name;
 	EXIT WHEN c1%NOTFOUND; 
-    dbms_output.put_line (x_dept_name); 
-    OPEN c2;
+    	dbms_output.put_line (x_dept_name); 
+		OPEN c2;
 		LOOP
-			FETCH c2 INTO x_id, x_name; 
-			EXIT WHEN c2%NOTFOUND; 
-			dbms_output.put_line ('       '||x_name);
-			x_tot_credit := 0;
-			x_gpa := 0; 
-			x_point := 0;
-			OPEN c3; 
+		FETCH c2 INTO x_id, x_name; 
+		EXIT WHEN c2%NOTFOUND;
+		dbms_output.put_line ('       '||x_name);
+		x_tot_credit := 0;
+		x_gpa := 0;
+		x_point := 0;
+			OPEN c3;
 			LOOP
-				FETCH c3 INTO x_title, x_credits, x_semester, x_year, x_grade;
-				EXIT WHEN c3%NOTFOUND;
-				dbms_output.put_line ('          '||x_title||' '|| x_credits||' '||x_semester||' '||x_year||' '||x_grade);
-				CASE x_grade
-					WHEN 'A+' THEN x_point := 4.3;
-					WHEN 'A' THEN x_point := 4; 
-					WHEN 'A-' THEN x_point := 3.7; 
-					WHEN 'B+' THEN x_point := 3.3; 
-					WHEN 'B' THEN x_point := 3; 
-					WHEN 'B-' THEN x_point := 2.7; 
-					WHEN 'C+' THEN x_point := 2.4;
-					WHEN 'C' THEN x_point := 2.1; 
-					WHEN 'C-' THEN x_point := 1.8; 
-					WHEN 'D+' THEN x_point := 1.5; 
-					WHEN 'D' THEN x_point := 1.2; 
-					WHEN 'D-' THEN x_point := 0.9;
-					WHEN 'F' THEN x_point := 0;
-					ELSE x_point := 0; x_credits := 0; -- grade NULL
-				END CASE;
+			FETCH c3 INTO x_title, x_credits, x_semester, x_year, x_grade;
+			EXIT WHEN c3%NOTFOUND;
+			dbms_output.put_line ('          '||x_title||' '|| x_credits||' '||x_semester||' '||x_year||' '||x_grade);
+			CASE x_grade
+				WHEN 'A+' THEN x_point := 4.3;
+				WHEN 'A' THEN x_point := 4; 
+				WHEN 'A-' THEN x_point := 3.7; 
+				WHEN 'B+' THEN x_point := 3.3; 
+				WHEN 'B' THEN x_point := 3; 
+				WHEN 'B-' THEN x_point := 2.7; 
+				WHEN 'C+' THEN x_point := 2.4;
+				WHEN 'C' THEN x_point := 2.1; 
+				WHEN 'C-' THEN x_point := 1.8; 
+				WHEN 'D+' THEN x_point := 1.5; 
+				WHEN 'D' THEN x_point := 1.2; 
+				WHEN 'D-' THEN x_point := 0.9;
+				WHEN 'F' THEN x_point := 0;
+				ELSE x_point := 0; x_credits := 0; -- grade NULL
+			END CASE;
 			x_tot_credit := x_tot_credit + x_credits; 
 			x_gpa := x_gpa + x_credits*x_point;
 			END LOOP;
@@ -78,7 +78,7 @@ BEGIN
 			dbms_output.put_line ('          Total Credits: '||x_tot_credit||' GPA: '||x_gpa);
 			CLOSE c3;
 		END LOOP;
-		close c2; 
+		CLOSE c2; 
 	END LOOP;
 	CLOSE c1; 
 END;
